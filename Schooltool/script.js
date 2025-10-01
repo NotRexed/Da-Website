afa7f6a9429e17a18ebbec6ee8432e368e05809b
NotRexed <therealnothingfn@gmail.com>
2025-10-01 18:21:14 -0400

    Update script.js

--- Schooltool/script.js
+++ Schooltool/script.js
@@ -150,8 +150,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const toggleThemeBtn = document.getElementById('toggle-theme');
 
     // CDN / paths
-    const defaultZones = "https://cdn.jsdelivr.net/gh/NotRexed/assets@latest/zone.json";
-    const fallbackZones = "https://cdn.jsdelivr.net/gh/NotRexed/assets@latest/zone.json";
+    const defaultZones = "https://cdn.jsdelivr.net/gh/NotRexed/assets@latest/all-games-zone.json";
+    const fallbackZones = "https://cdn.jsdelivr.net/gh/NotRexed/assets@latest/all-games-zone.json";
     const coverURL = "https://cdn.jsdelivr.net/gh/NotRexed/covers@main";
     const htmlURL = "https://cdn.jsdelivr.net/gh/NotRexed/html@main";
 
@@ -945,3 +945,4 @@ tvshowBtn.addEventListener("click", () => updateSlider("tv"));
 updateSlider("movie");
 
 });
+
